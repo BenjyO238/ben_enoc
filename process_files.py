e0177fe2fadef231f2688debabaa4ff9f85c7811
Ben Orlowitz <borlowitz@gmail.com>
2013-07-22 16:47:50 -0400

    optparse working not but sys is

--- process_files.py
+++ process_files.py
@@ -1,6 +1,5 @@
 __author__ = 'Orlowitz'
 
-#TODO add optparse stuff for commandline
 #TODO write to file then write to db
 #TODO try except
 
@@ -8,47 +7,68 @@ __author__ = 'Orlowitz'
 import os, sys, re
 import audit_log_2 as al
 import optparse
-import optparse
+
 
 
 
 #command line stuff
 parser = optparse.OptionParser()
-parser.add_option('-d', help='audit file directory', dest='audit_dir', default=False, action='store')
+parser.add_option('-d', help='audit file directory', dest='audit_dir')
 
 (opts, args) = parser.parse_args()
 
-audit_dir = opts.audit_dir #'./audit_files' #directory of audit files
+audit_dir = sys.argv[1] #opts.audit_dir #'./audit_files' #opts.audit_dir #'./audit_files' #directory of audit files
 
 files_to_process = os.listdir(audit_dir)
+# print files_to_process
 
 
-def process_dir(audit_files):
+
+def process_dir(audit_dir):
     logins = {} #holder for records to write
     os.chdir(audit_dir)
-    for file in audit_files:
-        log_file = open(file, 'rb')
-        log_f = log_file.read().split('\n')
-        data = al.process_file(log_f)
-        logins[file] = data
-        log_file.close()
+    for file in files_to_process:
+        if file != '.DS_Store':
+            log_file = open(file, 'rb')
+            log_f = log_file.read().split('\n')
+            data = al.process_file(log_f)
+            logins[file] = data
+            log_file.close()
     return logins
 
-
-log_entries = process_dir(files_to_process)
-
-
-log_in_data = open('audit_login_data.txt', 'a')
+# l = process_dir(audit_dir)
+# print l
 
 
-for key, value in log_entries.items():
-    # print key, value
-    if value:
-        line = [str(key), value[0].strftime("%Y-%m-%d %H:%M:%S"), str(value[1]), str(value[2])]
-        # print line
-        w_line = '%s, %s, %s,%s' % (line[0],line[1],line[2],line[3])
-        log_in_data.write(w_line + '\n')
 
-log_in_data.close()
+
+def write_entries():
+    try:
+        log_in_data = open('../audit_login_data.txt', 'a')
+    except IOError:
+        log_in_data = open('../audit_login_data.txt', 'w')
+
+    log_entries = process_dir(audit_dir)
+
+    for key, value in log_entries.items():
+        # print key, value
+        if value:
+            line = [str(key), value[0].strftime("%Y-%m-%d %H:%M:%S"), str(value[1]), str(value[2])]
+            # print line
+            w_line = '%s, %s, %s,%s' % (line[0],line[1],line[2],line[3])
+            log_in_data.write(w_line + '\n')
+
+    log_in_data.close()
+
+# write_entries()
+
+
+def main():
+    write_entries()
+
+
+if __name__ == '__main__':
+    main()
+
 
 
