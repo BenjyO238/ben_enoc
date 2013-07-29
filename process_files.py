40a114c6b4a93bbaa4787fd3a00079a1ba566a10
Ben Orlowitz <borlowitz@gmail.com>
2013-07-29 14:35:21 -0400

    local exadata changes

--- process_files.py
+++ process_files.py
@@ -8,7 +8,7 @@ import os, sys, re
 import audit_log_2 as al
 import optparse
 import shutil
-
+import time
 
 
 
@@ -19,7 +19,7 @@ parser.add_option('-d', help='audit file directory', dest='audit_dir')
 (opts, args) = parser.parse_args()
 
 audit_dir = sys.argv[1] #opts.audit_dir #'./audit_files' #opts.audit_dir #'./audit_files' #directory of audit files
-
+current_dir = os.getcwd()
 files_to_process = os.listdir(audit_dir)
 # print files_to_process
 
@@ -35,6 +35,7 @@ def process_dir(audit_dir):
             data = al.process_file(log_f)
             logins[file] = data
             log_file.close()
+    os.chdir(current_dir)
     return logins
 
 # l = process_dir(audit_dir)
@@ -44,17 +45,18 @@ def process_dir(audit_dir):
 
 
 def write_entries():
+    os.chdir(current_dir)
     try:
-        log_in_data = open('../audit_login_data.txt', 'a')
+        log_in_data = open('./audit_login_data.txt', 'a')
     except IOError:
-        log_in_data = open('../audit_login_data.txt', 'w')
+        log_in_data = open('./audit_login_data.txt', 'w')
 
     log_entries = process_dir(audit_dir)
 
     for key, value in log_entries.items():
         # print key, value
         if value:
-            line = [str(key), value[0].strftime("%Y-%m-%d %H:%M:%S"), str(value[1]), str(value[2])]
+            line = [str(key), str(value[0]), str(value[1]), str(value[2])]
             # print line
             w_line = '%s, %s, %s,%s' % (line[0],line[1],line[2],line[3])
             log_in_data.write(w_line + '\n')
@@ -64,14 +66,17 @@ def write_entries():
 # write_entries()
 
 def move_completed_files(files_to_move):
+    os.chdir(audit_dir)
     files = files_to_move
     for file in files:
-        shutil.move(file, '../processed_audit_files')
-
+        shutil.move(file, current_dir + '/processed_audit_files')
+    os.chdir(current_dir)
 
 def main():
     write_entries()
+    print 'Done writing entries. Now moving files.'
     move_completed_files(files_to_process) #move files once processed.
+    print 'Done processing audit files'
 
 
 if __name__ == '__main__':
